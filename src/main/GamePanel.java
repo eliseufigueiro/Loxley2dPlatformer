6b72d76ed62d931451e4963e59678ffbd6f7e6f6
Eliseu Figueiro <eliseufigueiro@gmail.com>
2023-07-09 14:54:56 -0300

    Add class MouseInputs e mapeamento clicked e moved

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,15 +1,22 @@
 package main;
 
 import inputs.KeybordInputs;
+import inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
 
+    private MouseInputs mouseInputs;
+
     public GamePanel() {
 
+        mouseInputs = new MouseInputs();
+
         addKeyListener(new KeybordInputs());
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
 
     public void paintComponent(Graphics g) {
